0879e77ef5d482e02344941e478973040174b7b3
Sean <sean.gadimoff@gmail.com>
2020-05-08 14:27:47 -0400

    loop through vehicles and individual CRs + fetch announcements complete

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -2,6 +2,7 @@ package runner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -60,14 +61,68 @@ System.out.println(current);
 			// move on if the auction contains 0 vehicles
 			if (current.contains("View all 0 vehicles")) continue;
 			
-// refresh the vehicle links
+			// refresh the vehicle links to avoid stale element exception
 System.out.println("Waiting for page to load...");
-waitForLoad(driver);
-
+			waitForLoad(driver);
 			viewVehicleLinks = driver.findElements(By.cssSelector("tr>th>a"));
 			
-			// click on the link to view all vehicles
+			// click on the link to view all vehicles within the auction
 			viewVehicleLinks.get(i).click();
+			
+			// wait for the page to complete loading
+			waitForLoad(driver);
+			
+			// fetch the list of all CR buttons (vehicles with CR)
+			List<WebElement> crLinks = driver.findElements(By.cssSelector("tr>td.make>div[name='sticker']>a.icon"));
+			
+			// for each CR link
+			for (int j = 0; j < crLinks.size(); j++) {
+				// store current window information
+				String parentWindow = driver.getWindowHandle();
+				
+System.out.println("Current window is: " + driver.getTitle());
+				
+				// click on the current CR link
+				crLinks.get(j).click();
+				try { Thread.sleep(5000); } catch(InterruptedException e) {}
+								
+				// fetch the newly opened window handle and switch to it
+				Set<String> handles =  driver.getWindowHandles();
+				for(String windowHandle  : handles) {
+					if(!windowHandle.equals(parentWindow)) {
+						driver.switchTo().window(windowHandle);
+						
+System.out.println("Current window after switch is: " + driver.getTitle());
+						
+						// wait for the new window to finish loading
+						waitForLoad(driver);
+						
+						// switch to the Frame where vehicle info is stored
+						driver.switchTo().frame("ecrFrame");
+						
+						// fetch the vehicle information
+						String vehicleTitle = driver.findElement(By.cssSelector("h2[class='ymmt-headline']")).getText();
+System.out.println("Opened the CR window for: " + vehicleTitle);						
+						
+						// fetch the vehicle ANNOUCEMENTS
+						String announcement = driver.findElement(By.id("cr_announcements")).getText();
+System.out.println("The ANNOUCEMENTS are: " + announcement);						
+						
+						// close the CR window
+						driver.close();
+						
+						// switch back to the parent window
+						driver.switchTo().window(parentWindow);
+					}
+				}
+				
+				
+				
+			}
+			
+			// TODO -> check for conditions statement of the announcement text
+			// TODO -> IF A MATCH, fetch vehicle info + auction info + concatenate the info to output String
+			// TODO -> AT THE END, email the output String with run date
 			
 System.out.println("Currently in : " + current);
 			
